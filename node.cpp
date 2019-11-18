ec0cad5c95e9ea50ef8fe8708274a52d6b07b619
Alexander Huber <dealex2007@web.de>
2019-11-18 10:59:18 +0100

    Update node.cpp
    Added modifi, remove and add methods for neighbors

--- node.cpp
+++ node.cpp
@@ -6,3 +6,24 @@ Uebung1::Node::Node(unsigned int id, string ipAddress, unsigned int port){
     setIpAddress(ipAddress);
     setPort(port);
 }
+
+
+Uebung1::Node Uebung1::Node::getNeighbor(unsigned int id){
+    return getNeighbors().at(id);
+}
+      
+void Uebung1::Node::addNeighbor(Node neighbor){
+    //TODO: Fehlerbehandlung
+    getNeighbors().insert(neighbor.getId(), neighbor);
+}
+
+void Uebung1::Node::removeNeighbor(unsigned int id){
+    //TODO: Fehlerbehandlung
+    getNeighbors().erase(id);
+}
+
+
+void Uebung1::Node::updateNeighbor(unsigned int id, Node neighbor){
+    //TODO: Fehlerbehandlung
+    getNeighbors().at(id) = neighbor;
+}
